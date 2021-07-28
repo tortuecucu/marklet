bbb08afba5a1d53f1a525ecf40f2eac5429ec412
tortuecucu <36691272+tortuecucu@users.noreply.github.com>
2021-07-28 17:10:37 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-alert('github hosted javascript');
+var d = document;
+navbars = d.getElementsByClassName('navbar_ui_actions');
+if (navbar.length > 0){
+  alert('navbar found');
+  var n = navbars[0]
+  let b = document.createElement("button");
+  n.appendChild(b);
+}
