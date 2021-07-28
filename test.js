fb9cc1f53ec41086a75d5724393c8f9dd21ae1c7
tortuecucu <36691272+tortuecucu@users.noreply.github.com>
2021-07-28 17:11:49 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var d = document;
 navbars = d.getElementsByClassName('navbar_ui_actions');
-if (navbar.length > 0){
+if (navbars.length > 0){
   alert('navbar found');
   var n = navbars[0]
   let b = document.createElement("button");
