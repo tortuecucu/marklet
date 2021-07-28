90a3b8235f5ab7b78fe8578d4820d921934064d6
tortuecucu <36691272+tortuecucu@users.noreply.github.com>
2021-07-28 17:14:50 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var d = document;
 navbars = d.getElementsByClassName('navbar_ui_actions');
 if (navbars.length > 0){
   alert('navbar found');
-  var n = navbars[0]
+  var n = navbars[0];
   let b = document.createElement("button");
   n.appendChild(b);
 }
